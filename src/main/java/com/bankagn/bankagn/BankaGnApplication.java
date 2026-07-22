4feb1284b24a42ee6e42bed861fea04976755a6f
Fatoumata-bDiallo <fatmabinta47@gmail.com>
2026-07-22 11:52:39 +0000

    Nettoyage BankaGnApplication

--- src/main/java/com/bankagn/bankagn/BankaGnApplication.java
+++ src/main/java/com/bankagn/bankagn/BankaGnApplication.java
@@ -2,16 +2,13 @@ package com.bankagn.bankagn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BankaGnApplication {
 
     public static void main(String[] args) {
-        // Afficher le hash de admin123
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("HASH = " + encoder.encode("admin123"));
-
         SpringApplication.run(BankaGnApplication.class, args);
     }
 }
