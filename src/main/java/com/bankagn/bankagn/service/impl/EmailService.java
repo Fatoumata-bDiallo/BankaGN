9e43489dff8f314f26c10c129c491e8e50d4a803
Fatoumata-bDiallo <fatmabinta47@gmail.com>
2026-07-22 14:22:06 +0000

    Retour Gmail SMTP

--- src/main/java/com/bankagn/bankagn/service/impl/EmailService.java
+++ src/main/java/com/bankagn/bankagn/service/impl/EmailService.java
@@ -1,35 +1,32 @@
 package com.bankagn.bankagn.service.impl;
 
-import com.resend.Resend;
-import com.resend.core.exception.ResendException;
-import com.resend.services.emails.model.CreateEmailOptions;
-import com.resend.services.emails.model.CreateEmailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
-    private final Resend resend;
+    private final JavaMailSender mailSender;
 
-    public EmailService() {
-        this.resend = new Resend("re_MkYnzo4C_CcTPRM9NwoqBBqFZEhh65aSj");
-    }
-
+    @Async
     public void envoyerEmail(String destinataire,
                              String sujet,
                              String message) {
         try {
-            CreateEmailOptions params = CreateEmailOptions.builder()
-                    .from("BankaGN <onboarding@resend.dev>")
-                    .to(destinataire)
-                    .subject(sujet)
-                    .text(message)
-                    .build();
-
-            CreateEmailResponse data = resend.emails().send(params);
-            System.out.println("✅ Email envoyé ! ID: " + data.getId());
-        } catch (ResendException e) {
-            System.err.println("❌ Erreur envoi email : " + e.getMessage());
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setFrom("fatmabinta47@gmail.com");
+            email.setTo(destinataire);
+            email.setSubject(sujet);
+            email.setText(message);
+            mailSender.send(email);
+            System.out.println("✅ Email envoyé à : " + destinataire);
+        } catch (Exception e) {
+            System.err.println("❌ Erreur envoi email : "
+                    + e.getMessage());
         }
     }
 }
